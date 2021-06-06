b68ab8e66864a9047d4fbd00ef7cac6384809863
Jim Blackler <jimblackler@google.com>
2021-06-06 15:54:41 +0100

    Fragments of Sheets API code (not working yet).

--- script.js
+++ script.js
@@ -6,5 +6,39 @@ const documentEditor = ace.edit(document.getElementById('documentEditor'), {
 
 const inputButton = document.getElementById('importButton');
 inputButton.addEventListener('click', event => {
-  alert('bo');
+  gapi.load('client:auth2', () => {
+    gapi.client.init({
+      apiKey: 'cf8e9ig-pgXCJLM-4h_8lkO7',
+      clientId: '559178029904-o2hg6atu0jgucuns8mgfn61532galeqk.apps.googleusercontent.com',
+      discoveryDocs: ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
+      scope: "https://www.googleapis.com/auth/drive.file"
+    }).then(() => {
+      gapi.auth2.getAuthInstance().isSignedIn.listen(isSignedIn => {
+        if (!isSignedIn) {
+          gapi.auth2.getAuthInstance().signIn();
+          return;
+        }
+
+        gapi.client.sheets.spreadsheets.values.get({
+          spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
+          range: 'Class Data!A2:E',
+        }).then(response => {
+          const range = response.result;
+          if (range.values.length > 0) {
+            console.log('Name, Major:');
+            for (let i = 0; i < range.values.length; i++) {
+              const row = range.values[i];
+              console.log(row[0] + ', ' + row[4]);
+            }
+          } else {
+            console.log('No data found.');
+          }
+        }, response => {
+          alert(response.result.error.message);
+        });
+      });
+    }, error => {
+      alert(JSON.stringify(error, null, 2));
+    });
+  });
 });
