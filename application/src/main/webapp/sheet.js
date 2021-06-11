37d0d949d02a0d42227f2ffdfadfb09c21c8dff1
Jim Blackler <jimblackler@google.com>
2021-06-12 00:05:31 +0100

    Spreadsheet vaue actually created.

--- application/src/main/webapp/sheet.js
+++ application/src/main/webapp/sheet.js
@@ -1,27 +1,39 @@
 export function getSheet(data) {
+  const keys = [];
+  data.forEach(row => {
+    for (const [key, value] of Object.entries(row)) {
+      if (!keys.includes(key)) {
+        keys.push(key);
+      }
+    }
+  });
+
+  const values = [];
+  keys.forEach(key => values.push({userEnteredValue: {stringValue: key}}));
+
+  const rowData = [{values}];
+  data.forEach(row => {
+    const rowValues = [];
+    keys.forEach(key => {
+      const rowElement = row[key];
+      if (rowElement === undefined) {
+        rowValues.push({});
+      } else {
+        if (typeof rowElement === 'number') {
+          rowValues.push({userEnteredValue: {numberValue: rowElement}});
+        } else if (typeof rowElement === 'boolean') {
+          rowValues.push({userEnteredValue: {boolValue: rowElement}});
+        } else {
+          rowValues.push({userEnteredValue: {stringValue: rowElement}});
+        }
+      }
+    });
+    rowData.push({values: rowValues});
+  });
+
   return {
     properties: {},
-    sheets: [{
-      properties: {gridProperties: {frozenRowCount: 1}},
-
-      data: [{
-        rowData: [
-          {
-            values: [
-              {userEnteredValue: {stringValue: 'a'}},
-              {userEnteredValue: {stringValue: 'b'}},
-              {userEnteredValue: {stringValue: 'c'}}
-            ]
-          },
-          {
-            values: [
-              {userEnteredValue: {numberValue: 1}},
-              {userEnteredValue: {numberValue: 2}},
-              {userEnteredValue: {numberValue: 3}}
-            ]
-          }
-        ]
-      }]
-    }]
+    sheets:
+        [{properties: {gridProperties: {frozenRowCount: 1}}, data: [{rowData}]}]
   };
 }
