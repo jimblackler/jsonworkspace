8931f987647ca2792f8863e024cd14c7650f22c4
Jim Blackler <jimblackler@google.com>
2021-12-15 11:14:48 +0000

    Null values now handled correctly.

--- application/src/main/webapp/flatten.js
+++ application/src/main/webapp/flatten.js
@@ -1,14 +1,16 @@
 function flatten(stem, inValue, consumer) {
-  if (inValue.constructor === Object) {
-    for (const [key, value] of Object.entries(inValue)) {
-      flatten(stem ? `${stem}.${key}` : key, value, consumer);
+  if (inValue !== null) {
+    if (inValue.constructor === Object) {
+      for (const [key, value] of Object.entries(inValue)) {
+        flatten(stem ? `${stem}.${key}` : key, value, consumer);
+      }
+      return;
     }
-    return;
-  }
-  if (Array.isArray(inValue)) {
-    inValue.forEach(
-        (row, index) => flatten(`${stem}[${index}]`, row, consumer));
-    return;
+    if (Array.isArray(inValue)) {
+      inValue.forEach(
+          (row, index) => flatten(`${stem}[${index}]`, row, consumer));
+      return;
+    }
   }
   consumer(stem, inValue);
 }
